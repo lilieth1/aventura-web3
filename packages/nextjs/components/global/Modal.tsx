6ba232ec2fb9509f09f72b5c464a941028886206
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2024-05-17 09:27:15 -0400

    feat(modal): add modal for end the course

--- packages/nextjs/components/global/Modal.tsx
+++ packages/nextjs/components/global/Modal.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Image from "next/image";
+import Logo from "./Logo/Logo";
 
 const Modal = ({ onClose }: any) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative bg-[#1a1a1a] rounded-lg p-8 shadow-lg w-96 text-white">
-        <div className="flex justify-end">
-          <button onClick={onClose} className="text-pink-500">
-            <Image src="/assets/close-icon.svg" alt="Close" width={24} height={24} />
-          </button>
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 p-32">
+      <div className="relative bg-[#1a1a1a] rounded-lg p-8 shadow-lg w-1/3 h-4/5 font-white">
+        <div className="flex justify-between">
+          <Logo />
+          <div className="flex justify-end" onClick={onClose}>
+            <Image src="/assets/course/cruz-close.svg" alt="Award" width={25} height={25} className="text-bold" />
+          </div>
         </div>
-        <div className="text-center">
-          <Image src="/assets/award-icon.svg" alt="Award" width={80} height={80} className="mx-auto mb-4" />
-          <h2 className="text-2xl font-bold mb-2">¡Felicidades!</h2>
-          <p className="text-lg mb-2">Completaste el módulo 1</p>
-          <p className="text-pink-500 text-lg mb-4">¡Ganaste 50 puntos!</p>
-          <p className="text-lg">¡Muy pronto el módulo 2!</p>
+        <div className="text-center mt-4">
+          <Image src="/assets/course/token.svg" alt="Award" width={200} height={200} className="mx-auto mb-4" />
+          <h2 className="text-3xl font-bold font-orbitron mb-2">¡Felicidades!</h2>
+          <p className="text-3xl font-bold font-orbitron mb-2">Completaste el módulo 1</p>
+          <p className="text-[#FF57F6] font-orbitron text-lg mb-4">¡Ganaste 50 puntos!</p>
+          <p className="text-lg font-orbitron">¡Muy pronto el módulo 2!</p>
         </div>
       </div>
     </div>
